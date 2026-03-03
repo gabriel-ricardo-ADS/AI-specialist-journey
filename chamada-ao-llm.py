6f9db7a322b11baa97de9331101c6d6a5303ce14
Gabriel Ricardo Gomes Lima <gabrielricardoglima@gmail.com>
2026-03-03 15:41:14 -0300

    Update chamada-ao-llm.py

--- chamada-ao-llm.py
+++ chamada-ao-llm.py
@@ -1,7 +1,29 @@
-print("ola")
+#após configurar o venv para baixar a biblioteca da OpenAI com '-m venv venv' e então 'pip install -q openai" 
+
+from openai import OpenAI
+
+client_openai = OpenaAI(
+
+    base_url = "http://127.0.0.1:1234/v1" #o 'v1' foi adicionado além do já gravado no LMstudio"
+    api_key = "lm-studio" #qualquer coisa
+
+)
+
+resposta_do_llm = client_openai.chat.completions.create(
+
+    model = "google/gemma-3-4b", #copiado do LMstudio após ter baixado e dado running
+    messages =  [
+        {"role":"system", "content": "Você é um assistente que responde de forma sarcastica"} #pode controlar a personalidade do modelo"
+        {"role":"user", "content": "O que é a IA Generativa?"}
+    ],
+    temperature = 1.0
+)   
+
+print(resposta_do_llm.choices[0].message.content) #para nao aparecer as listas e informações a mais
 
 
  
+
 
 
 
